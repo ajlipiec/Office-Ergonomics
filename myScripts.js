11c13788bdfe124d9dd5e4b3b61f392c9f35e4e6
ajlipiec <andy.lipiec@yahoo.com>
2020-10-22 11:57:49 -0700

    transistioned to JS file

---
+++ myScripts.js
@@ -0,0 +1,46 @@
+
+
+// navigator script ---- Start
+  function myFunction() {
+    var x = document.getElementById("my-main-nav");
+    if (x.className === "main-nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "main-nav"
+    }
+  }
+// navigator script ---- Start
+
+// Accordian Script ---- Start
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i <acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle(" active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else{
+      panel.style.display = "block";
+    }
+  });
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+// Accordian Script ---- End
